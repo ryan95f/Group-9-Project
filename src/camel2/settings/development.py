f50e9902df7b0bf03f4b112515c28ceff36108bb
Braden Marshall <braden1996@hotmail.co.uk>
2015-12-21 18:47:36 +0000

    Modulated the settings.

---
+++ src/camel2/settings/development.py
@@ -0,0 +1,11 @@
+"""
+Setting specific to running Camel in a development environment.
+"""
+
+try:
+    from settings.base import *  # NOQA
+except ImportError as e:
+    pass
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '*s&_3f625j!3h#re=j+qcq(t*hmg3y_28!8-l-l650abr1*+_*'
---
+++ src/camel2/settings/production.py
@@ -0,0 +1,17 @@
+"""
+Settings specific to running Camel in a production environment.
+
+Read before deployment:
+https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
+"""
+
+try:
+    from settings.base import *  # NOQA
+except ImportError as e:
+    pass
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# To keep us from pushing the deployed 'SECRET_KEY' into version control,
+# we store it in a local text-file that will not be commited.
+with open('/local/production_secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
