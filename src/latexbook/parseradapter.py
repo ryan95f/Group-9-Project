2840a53488e9b6e12643a51d885f86ea9af8ce47
Braden Marshall <braden1996@hotmail.co.uk>
2016-01-04 01:26:50 +0000

    Fixed a few silly bugs.

--- src/latexbook/parseradapter.py
+++ src/latexbook/parseradapter.py
@@ -6,17 +6,17 @@ def write_to_django_database(nodes, node, parent_node=None):
     This function adds the children of 'node' through recursive calls.
     """
     # Check if the node is a TextNode.
-    if node.text_node_id == node.get_id():
+    if nodes.text_node_id == node.get_id():
         text_node = TextNode.objects.create(parent=parent_node, position=node.position, text=node.content)
 
         # Save to Django database.
         text_node.save()
     else:
-        book_node = BookNode.objects.create(parent=parent_node, position=node.position, node_type=node.get_type())
+        book_node = BookNode.objects.create(parent=parent_node, position=node.position, node_type=node.get_id())
 
         # Save to Django database.
         book_node.save()
 
         # Although a TextNode can't have children, a regular node could have.
         for child in node.children:
-             write_to_django_database(nodes=nodes, node=child, parent=book_node)
+            write_to_django_database(nodes=nodes, node=child, parent_node=book_node)
