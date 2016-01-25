802c30a10e83c3e37b2a0cb46f5c1f5e85bf5f8c
Braden Marshall <braden1996@hotmail.co.uk>
2016-01-25 22:45:33 +0000

    Added basic test demonstration for pulling and viewing the data from BookNode.

--- src/latexbook/views.py
+++ src/latexbook/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from latexbook.models import BookNode
+
+
+class BookNodeDetail(DetailView):
+    """"""
+    model = BookNode
+
+    def get_context_data(self, **kwargs):
+        context = super(BookDetailView, self).get_context_data(**kwargs)
+        book = self.get_object()
+        context["book"] = book
+        context["chapters"] = book.tree.get_descendants().filter(node_type="chapter")
+        return context
+
+
+def show_test(request):
+    return render_to_response("latexbook/test.html",
+                          {"nodes": BookNode.objects.all()},
+                          context_instance=RequestContext(request))
