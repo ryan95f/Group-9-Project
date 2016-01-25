2b6828b64d02e6ade5523c2a627fbecf90615dd7
Braden Marshall <braden1996@hotmail.co.uk>
2016-01-25 22:44:07 +0000

    Added BookNode plain-text content field.

--- src/latexbook/models.py
+++ src/latexbook/models.py
@@ -22,17 +22,10 @@ class BookNode(MPTTModel):
     # The type of this node. e.g. Chapter, Section, Verbatim, TextIt, ArgumentNode etc...
     node_type = models.CharField(max_length=50)
 
+    # The plain-text, if applicable, attached to this node.
+    # I feel like there should be seperate model for TextNode, but I can't figure out
+    # how to then 'insert' the text back into the tree - whilst retaining the order.
+    content = models.TextField(null=True, blank=True)
+
     class MPTTMeta:
         order_insertion_by = ["position"]
-
-
-class TextNode(models.Model):
-    """A leaf node whos parent is BookNode. Contains simply plain-text."""
-    # The parent this node belongs to.
-    parent = models.ForeignKey(BookNode, blank=True, null=True, on_delete=models.CASCADE)
-
-    # The position in which the node appears inside the parent.
-    position = models.IntegerField()
-
-    # The plain-text attached to this leaf-node.
-    text = models.TextField()
