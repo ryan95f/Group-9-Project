2e88c6b43c8c4d8dce129f4a354fc7a2f0a218c3
Braden Marshall <braden1996@hotmail.co.uk>
2016-01-28 21:04:14 +0000

    Some experimental progress towards development of the views.

--- src/latexbook/urls.py
+++ src/latexbook/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
-from latexbook.views import show_test
+from latexbook.views import BookNodeDetailView, BookNodeChapterDetailView, show_test
 
 urlpatterns = [
-    url(r"test/$", show_test, name="latexbook_booknode_detail")
+    url(r"^(?P<pk>[0-9]+)/$", BookNodeDetailView.as_view(), name="latexbook_booknode_detail"),
+    url(r"^(?P<book_pk>[0-9]+)/(?P<pk>[0-9]+)/$", BookNodeChapterDetailView.as_view(), name="latexbook_booknode_chapter_detail"),
+    url(r"test/$", show_test, name="latexbook_test")
 ]
--- src/latexbook/views.py
+++ src/latexbook/views.py
@@ -1,23 +1,41 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render
 from django.template import RequestContext
 from django.views.generic.detail import DetailView
 
 from latexbook.models import BookNode
 
 
-class BookNodeDetail(DetailView):
-    """"""
+class BookNodeDetailView(DetailView):
+    """Displays a list of all the chapters for the passed book."""
     model = BookNode
+    template_name = "latexbook/booknode_detail_view.html"
 
     def get_context_data(self, **kwargs):
-        context = super(BookDetailView, self).get_context_data(**kwargs)
+        context = super(BookNodeDetailView, self).get_context_data(**kwargs)
         book = self.get_object()
+        print(book)
         context["book"] = book
-        context["chapters"] = book.tree.get_descendants().filter(node_type="chapter")
+        context["chapters"] = book.get_descendants().filter(node_type="chapter")
+        return context
+
+
+class BookNodeChapterDetailView(DetailView):
+    """Displays the contents of the specified book chapter."""
+    model = BookNode
+    template_name = "latexbook/booknode_chapter_detail_view.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(BookNodeChapterDetailView, self).get_context_data(**kwargs)
+        chapter = self.get_object()
+        context["book"] = chapter.parent
+        context["chapter"] = chapter
         return context
 
 
 def show_test(request):
-    return render_to_response("latexbook/test.html",
-                          {"nodes": BookNode.objects.all()},
-                          context_instance=RequestContext(request))
+    return render(
+        request,
+        "latexbook/recurselatextree.html",
+        {"root_node": BookNode.objects.all()},
+        content_type="application/xhtml+xml"
+    )
