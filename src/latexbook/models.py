e9d8705c8d6e6a3000f0321879c30eebcef1335f
Braden Marshall <braden1996@hotmail.co.uk>
2016-01-28 21:02:25 +0000

    Added more of the required models.

--- src/latexbook/models.py
+++ src/latexbook/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 from mptt.models import MPTTModel, TreeForeignKey
 
+
 class BookNode(MPTTModel):
     """Represents a node inside a book."""
 
@@ -22,10 +23,46 @@ class BookNode(MPTTModel):
     # The type of this node. e.g. Chapter, Section, Verbatim, TextIt, ArgumentNode etc...
     node_type = models.CharField(max_length=50)
 
-    # The plain-text, if applicable, attached to this node.
-    # I feel like there should be seperate model for TextNode, but I can't figure out
-    # how to then 'insert' the text back into the tree - whilst retaining the order.
-    content = models.TextField(null=True, blank=True)
-
     class MPTTMeta:
         order_insertion_by = ["position"]
+
+
+class TextNode(models.Model):
+    """Associates some plain-text to a 'text' node from the BookNode model."""
+    # The id of the 'text' node in the BookNode model.
+    book_node = models.OneToOneField(
+        BookNode,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name="text_node"
+    )
+
+    # The actual plain-text for the node.
+    content = models.TextField()
+
+
+class Module(models.Model):
+    """A module."""
+
+    code = models.CharField(primary_key=True, max_length=6)
+
+    title = models.CharField(max_length=64)
+
+
+class Book(models.Model):
+    """A book."""
+    title = models.CharField(max_length=64)
+
+    # Preferably, this would be a ManyToMany relation to teacher users.
+    author = models.CharField(max_length=64)
+
+    # We use ManyToMany as a Book could be in multiple modules and a module can
+    # have multiple books.
+    module_codes = models.ManyToManyField(Module)
+
+    book_root_node = models.OneToOneField(
+        BookNode,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name="book"
+    )
