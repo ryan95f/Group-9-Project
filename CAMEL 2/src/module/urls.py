89528eccd7908f989098e1c5f794b694b26c7eca
Ryan95Z <Ryan95F@gmail.com>
2016-01-30 17:51:04 +0000

    module looks to see if any modules exist.

--- CAMEL 2/src/module/urls.py
+++ CAMEL 2/src/module/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'module'
 urlpatterns = [
     url(r'^$', views.module_index, name='module_index'),
+    url(r'^(?P<pk>\w+)/$', views.module_detail, name="module_detail"),
 ]
--- CAMEL 2/src/module/views.py
+++ CAMEL 2/src/module/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.core.urlresolvers import reverse
 
 from latexbook.models import Module
 
 def module_index(request):
 	return render(request,'module/module_index.html', {'modules' : Module.objects.all()})
+
+def module_detail(request, pk):
+	module = get_object_or_404(Module, pk=pk)
+	return render(request,'module/module_detail.html', {'module' : module })
+
