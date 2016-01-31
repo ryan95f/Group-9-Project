c98e61e4b26fad795707532917a498bf5f9cbc42
Ryan95Z <Ryan95F@gmail.com>
2016-01-31 12:23:43 +0000

    started to add books to the modules

--- CAMEL 2/src/module/urls.py
+++ CAMEL 2/src/module/urls.py
@@ -5,4 +5,5 @@ app_name = 'module'
 urlpatterns = [
     url(r'^$', views.module_index, name='module_index'),
     url(r'^(?P<pk>\w+)/$', views.module_detail, name="module_detail"),
+    url(r'^book/(?P<pk>\d+)/$', views.book, name="book")
 ]
--- CAMEL 2/src/module/views.py
+++ CAMEL 2/src/module/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
 
 from module.models import Module
+from latexbook.models import Book
 
 
 def module_index(request):
@@ -11,4 +12,8 @@ def module_index(request):
 def module_detail(request, pk):
 	module = get_object_or_404(Module, pk=pk)
 	return render(request,'module/module_detail.html', {'module' : module })
+
+def book(request, pk):
+	book = get_object_or_404(Book, pk=pk)
+	return render(request, 'module/book.html', {'book' : book })
 
