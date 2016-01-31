78131b9cf14b3dd620e0c7b24de4a63f5bcc9d72
Braden Marshall <braden1996@hotmail.co.uk>
2016-01-31 18:31:12 +0000

    Improved semantic meaning of URLs.

--- CAMEL 2/src/latexbook/urls.py
+++ CAMEL 2/src/latexbook/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from latexbook.views import BookNodeDetailView, BookNodeChapterDetailView, show_test
 
 urlpatterns = [
-    url(r"^(?P<pk>[0-9]+)/$", BookNodeDetailView.as_view(), name="latexbook_booknode_detail"),
-    url(r"^(?P<book_pk>[0-9]+)/(?P<pk>[0-9]+)/$", BookNodeChapterDetailView.as_view(), name="latexbook_booknode_chapter_detail"),
+    url(r"^book/(?P<pk>[0-9]+)/$", BookNodeDetailView.as_view(), name="latexbook_booknode_detail"),
+    url(r"^book/(?P<book_pk>[0-9]+)/chapter/(?P<pk>[0-9]+)/$", BookNodeChapterDetailView.as_view(), name="latexbook_booknode_chapter_detail"),
     url(r"test/$", show_test, name="latexbook_test")
 ]
