e25a19355bdb1349293ae3e2bd8c38e32d41e261
Braden Marshall <braden1996@hotmail.co.uk>
2016-02-03 12:56:48 +0000

    Added app_name for URL Namespace.

--- CAMEL 2/src/latexbook/urls.py
+++ CAMEL 2/src/latexbook/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 
 from latexbook.views import BookNodeDetailView, BookNodeChapterDetailView, show_test
 
+app_name = 'latexbook'
 urlpatterns = [
-    url(r"^book/(?P<pk>[0-9]+)/$", BookNodeDetailView.as_view(), name="latexbook_booknode_detail"),
-    url(r"^book/(?P<book_pk>[0-9]+)/chapter/(?P<pk>[0-9]+)/$", BookNodeChapterDetailView.as_view(), name="latexbook_booknode_chapter_detail"),
-    url(r"test/$", show_test, name="latexbook_test")
+    url(r"^book/(?P<pk>[0-9]+)/$", BookNodeDetailView.as_view(), name="booknode_detail"),
+    url(r"^book/(?P<book_pk>[0-9]+)/chapter/(?P<pk>[0-9]+)/$", BookNodeChapterDetailView.as_view(), name="booknode_chapter_detail"),
+    url(r"test/$", show_test, name="test")
 ]
