74e2a24818ceff51e5338a663f4c4fb5c028933f
Braden Marshall <braden1996@hotmail.co.uk>
2016-03-24 17:00:54 +0000

    Created base item class.

--- CAMEL 2/src/camelcore/latexparser/nodes/commands/item.py
+++ CAMEL 2/src/camelcore/latexparser/nodes/commands/item.py
@@ -1,7 +1,15 @@
 from latexbook.latexparser.nodes.command import CommandNode
 
 
-class Item(CommandNode):
+class ItemNode(CommandNode):
+    """An abstract item command node."""
+
+    def __init__(self, children=None):
+        """Initialise the node."""
+        super(ItemNode, self).__init__(children=children)
+
+
+class Item(ItemNode):
     """An item command node."""
 
     def __init__(self, children=None):
