8b88c52d8b8ac06bef02d2622951dd07f927d7b5
Braden Marshall <braden1996@hotmail.co.uk>
2016-03-24 18:07:28 +0000

    Added a few notes.

--- CAMEL 2/src/homeworkquiz/models.py
+++ CAMEL 2/src/homeworkquiz/models.py
@@ -1,6 +1,30 @@
 from django.db import models
-from user.models import CamelUser
 
+# We shouldn't be directly importing any of our other apps.
+# It contradicts the fact that our apps are meant to be decoupled; a point we have fairly emphasised throughout our
+# reports.
+# Each one of our project's apps - except camelcore - should be integratable into other Django projects.
+# This requires us to focus on two things:
+# - Do not create dependencies with other apps (unless the alternative makes absolutely no sense).
+# - Make relations generic where necessary.
+#
+# If we stick with the Django mindset of reusability & decoupled apps, we'll probably have to modify the user app as
+# it holds a dependecy with Camel. Maybe we should leave the user app purely for its login/logout functionality
+# and some additional user fields. All the staff related stuff should only exist within the camelcore app. I think
+# it'll also be a neat idea if the homeworkquiz app would define the permissions for being able to create tests etc...
+#
+# Also, whilst I'm typing here, if you get a spare few minutes, would you mind taking a look at:
+# 'CAMEL 2/src/latexbook/templatetags/latex_html.py :: get_template_node(...)'
+# I wanted:
+# 'CAMEL 2/src/camel2/templates/camelcore/homeworkquiz/latexbook/latexparser/nodes/commands/choice/correctchoice.html'
+# to simply extend from:
+# 'CAMEL 2/src/camel2/templates/camelcore/homeworkquiz/latexbook/latexparser/nodes/commands/choice/choice.html'
+# But it was just giving me a fuck load of errors.
+#
+# Django's contenttypes framework is quite handy for this kind of stuff! :)
+# https://docs.djangoproject.com/en/1.9/ref/contrib/contenttypes/
+
+from user.models import CamelUser
 from latexbook.models import BookNode
 
 
