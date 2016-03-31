dfd5d048c62ed5604cde517b023271dd86a86130
Ryan95Z <Ryan95F@gmail.com>
2016-03-31 21:13:24 +0100

    added admin part of questions, made a base answer model

--- CAMEL 2/src/homeworkquiz/admin.py
+++ CAMEL 2/src/homeworkquiz/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from homeworkquiz.models import SingleChoiceAnswer, JaxAnswer
 
-admin.site.register(SingleChoiceAnswer)
-admin.site.register(JaxAnswer)
+
+class AnswerAdmin(admin.ModelAdmin):
+    """Model for answers to be displayed in Django Admin panel. Applied t
+    all answers types. Restricts what can be edited by Admin"""
+    list_display = ('user', 'created', 'question_node_id', 'is_submitted')
+    list_filter = ('is_submitted', 'created')
+    fieldsets = (
+        ('User Answer', {'fields': ('answer',)}),
+        ('Permissions', {'fields': ('is_submitted',)}),
+    )
+
+admin.site.register(SingleChoiceAnswer, AnswerAdmin)
+admin.site.register(JaxAnswer, AnswerAdmin)
--- CAMEL 2/src/homeworkquiz/models.py
+++ CAMEL 2/src/homeworkquiz/models.py
@@ -29,35 +29,39 @@ from user.models import CamelUser
 # https://docs.djangoproject.com/en/1.9/ref/contrib/contenttypes/
 
 
-class SingleChoiceAnswer(models.Model):
-    """Model for holding the data that is related
-    to an answer that is entered by a user for Single Choice"""
-    # note: will update to make it more generic
-    # Done this way to get logic understood
-    answer = models.CharField(max_length=20)
-    user = models.ForeignKey(CamelUser, default=None)
-    node = models.ForeignKey(BookNode, default=None)
-    created = models.DateTimeField(auto_now=True)
-    save_date = models.DateTimeField(auto_now=True)
-    submitted_date = models.DateTimeField(auto_now=True)
-    is_submitted = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.answer
-
-
-class JaxAnswer(models.Model):
-    """Model for holding the answer that is entered
-    for a mathjax answer"""
-    # note: will update to make it more generic
-    # Done this way to get logic understood
-    answer = models.CharField(max_length=100)
+class GenericAnswerModel(models.Model):
+    """Concrate model that contains the core variables
+    for storing a students answer. Is inherited to allow
+    other specific models access."""
     user = models.ForeignKey(CamelUser, default=None)
     node = models.ForeignKey(BookNode, default=None)
     created = models.DateTimeField(auto_now_add=True)
     save_date = models.DateTimeField(auto_now=True)
     submitted_date = models.DateTimeField(auto_now_add=True)
     is_submitted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+    def question_node_id(self):
+        return self.node.pk
+
+
+class SingleChoiceAnswer(GenericAnswerModel):
+    """Model for holding the data that is related
+    to an answer that is entered by a user for Single Choice.
+    Inherits GenericAnswerModel which contains core information"""
+    answer = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.answer
+
+
+class JaxAnswer(GenericAnswerModel):
+    """Model for holding the answer that is entered
+    for a mathjax answer. Inherits GenericAnswerModel which
+    contains core information"""
+    answer = models.CharField(max_length=100)
 
     def __str__(self):
         return self.answer
