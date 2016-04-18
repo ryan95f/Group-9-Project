5f7e4c2a2fa6505f4690922f0fd602dff5a5c14e
Ryan95Z <Ryan95F@gmail.com>
2016-04-18 18:25:44 +0100

    Removed un-needed files

--- Archive/CAMEL2/user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name = 'user'
-urlpatterns = [
-    url(r'login/', views.login_view, name='login'),
-    url(r'^home/(?P<pk>\d+)/$', views.userhome, name="user_home"),
-    url(r'^logout/$', views.logout_view, name="logout"),
-]
