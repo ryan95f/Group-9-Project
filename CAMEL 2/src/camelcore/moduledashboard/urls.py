20a30b4bce6fd944eea76948abc171110b045467
Ryan95Z <Ryan95F@gmail.com>
2016-04-20 10:34:34 +0100

    some extra security

--- CAMEL 2/src/camelcore/moduledashboard/urls.py
+++ CAMEL 2/src/camelcore/moduledashboard/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 
 from .views import book_add_detail_view, book_add_view, book_create_view, book_delete_view
 
 app_name = "camelcore"
 
 urlpatterns = [
-    url(r"^(?P<module_pk>\w+)/add/book/$", book_add_detail_view, name="add_detail_book"),
-    url(r"^(?P<module_pk>\w+)/add/book/(?P<book_pk>\w+)/$", book_add_view, name="add_book"),
-    url(r"^(?P<module_pk>\w+)/create/book/$", book_create_view, name="create_book"),
-    url(r"^(?P<module_pk>\w+)/delete/book/(?P<book_pk>\w+)/$", book_delete_view, name="delete_book"),
+    url(r"^(?P<module_pk>\w+)/add/book/$", login_required(book_add_detail_view), name="add_detail_book"),
+    url(r"^(?P<module_pk>\w+)/add/book/(?P<book_pk>\w+)/$", login_required(book_add_view), name="add_book"),
+    url(r"^(?P<module_pk>\w+)/create/book/$", login_required(book_create_view), name="create_book"),
+    url(r"^(?P<module_pk>\w+)/delete/book/(?P<book_pk>\w+)/$", login_required(book_delete_view), name="delete_book"),
 ]
