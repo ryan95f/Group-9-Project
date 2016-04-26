9695c01545a76867c3402cf73fcc395340014b09
Ryan95Z <Ryan95F@gmail.com>
2016-04-26 16:21:48 +0100

    fixed mathjax not loading for questions

---
+++ UI/CAMEL 2/src/latexbook/migrations/0015_auto_20160425_1815.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('latexbook', '0014_auto_20160425_1722'),
+    ]
+
+    operations = [
+        migrations.AlterModelManagers(
+            name='booknode',
+            managers=[
+            ],
+        ),
+    ]
