8d84badb8a3af8626fc9793d4bedc111c0f1eb92
Sh'kyra Jordon <ShkyraNJordon@hotmail.com>
2016-04-28 13:48:02 +0100

    Single answer jquery

---
+++ CAMEL 2/src/latexbook/migrations/0014_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('latexbook', '0013_auto_20160422_1148'),
+        ('latexbook', '0013_auto_20160422_1848'),
+    ]
+
+    operations = [
+    ]
